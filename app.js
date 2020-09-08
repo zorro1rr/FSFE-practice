a485264373d34c34d539f561dbce09f382891a6f
zorro1rr <zorro1rr@gmail.com>
2020-09-08 15:29:01 -0600

    4xx response practice

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-	res.send('Hello World');
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+app.get("/demo", (req, res) => {
+  res.set("Justin likes to code");
+  res.status(418);
+  res.send("Justin is a cool dude");
+});
